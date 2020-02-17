511a97d82a86db3b96c4885d79c6279865db6841
rahulgidde <rahulgidde1212@gmail.com>
2020-02-17 15:36:32 +0530

    ADDED:Flip a Coin and Check Head or Tail

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,3 +1,14 @@
 #!/bin/bash -x
 
 echo "-------------Welcome to Flip Coin Simulation----------"
+
+#GENRATE RANDOM NUMBER
+randomcheck=$((RANDOM%2))
+
+#FLIP A COIN AND CHECK HEAD OR TAIL
+if [ $randomcheck -eq 1 ]
+then
+	echo "Head"
+else
+	echo "Tail"
+fi
