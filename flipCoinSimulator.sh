2940a5361c9749793871d44834f71321495de361
rahulgidde <rahulgidde1212@gmail.com>
2020-02-19 12:07:44 +0530

    ADDED:Store the doublet combination and percentage

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -2,6 +2,9 @@
 
 #TAKE INPUT FROM USER
 read -p "Enter The Number Of Times Coin Flip: " number
+echo "1.Singlet"
+echo "2.Doublet"
+read -p "Enter Your Choice:" choice
 
 #DECLARTION OF DICTIONARY
 declare -A flipCoin
@@ -9,27 +12,56 @@ declare -A flipCoin
 #CONSTANT
 ISFLIP=1
 SINGLET=1
+DOUBLET=2
 
 #FLIP A COIN AND CHECK HEAD OR TAIL
-for (( index1=0; index1<$number; index1++ ))
-do
-	for (( index2=0; index2<$SINGLET; index2++ ))
+function getflip
+{
+	for (( index1=0; index1<$number; index1++ ))
 	do
-		randomcheck=$((RANDOM%2))
-		if [ $randomcheck -eq $ISFLIP ]
+		for (( index2=0; index2<$choice; index2++ ))
+		do
+			randomcheck=$((RANDOM%2))
+			if [ $randomcheck -eq $ISFLIP ]
+			then
+				coinSide+="H"
+			else
+				coinSide+="T"
+			fi
+		done
+		if [ $choice -eq $SINGLET ]
 		then
-			coinSide+="H"
-		else
-			coinSide="T"
+			((flipCoin[$coinSide]++))
+		elif [ $choice -eq $DOUBLET ]
+		then
+			((flipCoin[$coinSide]++))
 		fi
+		coinSide=""
+		echo "Count of all combination:${flipCoin[@]}"
 	done
-	((flipCoin[$coinSide]++))
-	coinSide=""
-done
+}
 
-#CALCULATE THE PERCENTAGE OF HEAD AND TAIL
-for index in ${!flipCoin[@]}
-do
-	flipCoin[$index]=`echo "scale=2; ${flipCoin[$index]}*100/$number" | bc`
-done
+#CALCULATE THE PERCENTAGE OF COMBINATION
+function getPercentage
+{
+	for index in ${!flipCoin[@]}
+	do
+		flipCoin[$index]=`echo "scale=2; ${flipCoin[$index]}*100/$number" | bc`
+	done
+	echo ${flipCoin[@]}
+}
 
+#FUNCTION CALL
+case $choice in
+	$SINGLET)
+		getflip
+		getPercentage
+	   ;;
+	$DOUBLET)
+		getflip
+		getPercentage
+		;;
+	*)
+		echo "Invalid choice:"
+		;;
+esac
