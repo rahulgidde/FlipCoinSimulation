a6411ae7336a265f5f278e82c67984ef74d6ec48
rahulgidde <rahulgidde1212@gmail.com>
2020-02-19 11:18:02 +0530

    ADDED:Store Singlet Combination in dictionary

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,14 +1,35 @@
 #!/bin/bash -x
 
-echo "-------------Welcome to Flip Coin Simulation----------"
+#TAKE INPUT FROM USER
+read -p "Enter The Number Of Times Coin Flip: " number
 
-#GENRATE RANDOM NUMBER
-randomcheck=$((RANDOM%2))
+#DECLARTION OF DICTIONARY
+declare -A flipCoin
+
+#CONSTANT
+ISFLIP=1
+SINGLET=1
 
 #FLIP A COIN AND CHECK HEAD OR TAIL
-if [ $randomcheck -eq 1 ]
-then
-	echo "Head"
-else
-	echo "Tail"
-fi
+for (( index1=0; index1<$number; index1++ ))
+do
+	for (( index2=0; index2<$SINGLET; index2++ ))
+	do
+		randomcheck=$((RANDOM%2))
+		if [ $randomcheck -eq $ISFLIP ]
+		then
+			coinSide+="H"
+		else
+			coinSide="T"
+		fi
+	done
+		((flipCoin[$coinSide]++))
+		coinSide=""
+done
+
+#CALCULATE THE PERCENTAGE OF HEAD AND TAIL
+for index in ${!flipCoin[@]}
+do
+	flipCoin[$index]=`echo "scale=2; ${flipCoin[$index]}*100/$number" | bc`
+done
+
